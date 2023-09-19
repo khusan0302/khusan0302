66779f9678f0d85e217f6ed8be3018f95862ea08
khusan0302 <106137852+khusan0302@users.noreply.github.com>
2023-09-19 19:59:34 +0900

    Add files via upload

---
+++ covid19_statistics.py
@@ -0,0 +1,42 @@
+def normalize_data(n_cases, n_people, scale):
+    # TODO: Calculate the number of cases per its population
+    norm_cases = []
+    for idx, n in enumerate(n_cases):
+        norm_cases.append(n*scale/n_people[idx])
+    return norm_cases
+
+
+regions = ['Seoul', 'Gyeongi', 'Busan', 'Gyeongnam', 'Incheon', 'Gyeongbuk', 'Daegu', 'Chungnam',
+           'Jeonnam', 'Jeonbuk', 'Chungbuk', 'Gangwon', 'Daejeon', 'Gwangju', 'Ulsan', 'Jeju', 'Sejong']
+n_people = [9550227,  13530519, 3359527,     3322373,   2938429,     2630254, 2393626,    2118183,
+            1838353,   1792476,    1597179,   1536270,   1454679,   1441970, 1124459, 675883,   365309]  # 2021-08
+n_covid = [644,       529,      38,          29,       148,          28,      41,         62,
+           23,        27,         27,        33,        16,        40,      20,      5,        4]  # 2021-09-21
+
+sum_people = sum(n_people)
+sum_covid = sum(n_covid)
+# The new cases per 1 million people
+norm_covid = normalize_data(n_covid, n_people, 1000000)
+
+# Print population by region
+print('### Korean Population by Region')
+print('* Total population:', sum_people)
+print()  # Print an empty line
+print('| Region | Population | Ratio (%) |')
+print('| ------ | ---------- | --------- |')
+for idx, pop in enumerate(n_people):
+    ratio = (pop/sum_people)*100  # TODO: The ratio of new cases to the total
+    print('| %s | %d | %.1f |' % (regions[idx], pop, ratio))
+print()
+
+# TODO: Print COVID-19 new cases by region
+print('### Korean COVID-19 New Cases by Region')
+print('* Total new cases:', sum_covid)
+print()  # Print an empty line
+print('| Region | New Cases | Ratio (%) | New Cases / 1M |')
+print('| ------ | ---------- | --------- | -------------- |')
+for idx, case in enumerate(n_covid):
+    ratio = (case/sum_covid)*100  # TODO: The ratio of new cases to the total
+    print('| %s | %d | %.1f | %.1f |' %
+          (regions[idx], case, ratio, norm_covid[idx]))
+print()
