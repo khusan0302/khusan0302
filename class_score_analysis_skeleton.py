b9458b9cf5d5d7691ad8567ba934d450fe0e0080
khusan0302 <106137852+khusan0302@users.noreply.github.com>
2023-09-24 23:47:49 +0900

    Add files via upload

---
+++ class_score_analysis_skeleton.py
@@ -0,0 +1,62 @@
+def read_data(filename):
+    # TODO) Read filename as a list of integer numbers
+    data = []
+    with open(filename, 'r') as file:
+        for line in file.readlines():
+            if line.startswith('#'):
+                continue
+            numbers = [int(text) for text in line.split(',')]
+            data.append(numbers)
+    return data
+
+def calc_weighted_average(data_2d, weights):
+    # TODO) Calculate the weighted averages of each row of data_2d
+    average = []
+    for row in data_2d:
+        weighted_sum = sum(x * w for x, w in zip(row, weights))
+        weighted_average = weighted_sum / sum(weights)
+        average.append(weighted_average)
+    return average
+
+def analyze_data(data_1d):
+    # TODO) Derive summary of the given data_1d
+    # Note) Please don't use NumPy and other libraries. Do it yourself.
+    mean = sum(data_1d) / len(data_1d)
+    var = sum((x - mean) ** 2 for x in data_1d) / len(data_1d)
+    median = 0
+    sorted_data = sorted(data_1d)
+    n = len(sorted_data)
+    if n % 2 == 0:
+        median = (sorted_data[n // 2 - 1] + sorted_data[n // 2]) // 2
+    else:
+        median = sorted_data[n // 2]
+
+    return mean, var, median, min(data_1d), max(data_1d)
+
+if __name__ == '__main__':
+    filename = 'data/class_score_en.csv'
+    data = read_data(filename)
+    if data and len(data[0]) == 2: # Check 'data' is valid
+        average = calc_weighted_average(data, [40/125, 60/100])
+
+        # Write the analysis report as a markdown file
+        with open('class_score_analysis.md', 'w') as report:
+            report.write('### Individual Score\n\n')
+            report.write('| Midterm | Final | Total |\n')
+            report.write('| ------- | ----- | ----- |\n')
+            for ((m_score, f_score), a_score) in zip(data, average):
+                report.write(f'| {m_score} | {f_score} | {a_score:.3f} |\n')
+            report.write('\n\n\n')
+
+            report.write('### Examination Analysis\n')
+            data_columns = {
+                'Midterm': [m_score for m_score, _ in data],
+                'Final'  : [f_score for _, f_score in data],
+                'Average': average }
+            for name, column in data_columns.items():
+                mean, var, median, min_, max_ = analyze_data(column)
+                report.write(f'* {name}\n')
+                report.write(f'  * Mean: {mean:.3f}\n')
+                report.write(f'  * Variance: {var:.3f}\n')
+                report.write(f'  * Median: {median:.3f}\n')
+                report.write(f'  * Min/Max: ({min_:.3f}, {max_:.3f})\n')
